933d769fa692d26ca79ccc482d1cf824a9310e5f
roma3 <roma38lorde@gmail.com>
2021-10-03 12:44:44 -0400

    created third class adding text

--- src/setup/Third.java
+++ src/setup/Third.java
@@ -3,5 +3,6 @@ package setup;
 public class Third {
     public static void main(String[] args) {
         System.out.println("create the third file");
+        System.out.println("Add some text");
     }
 }

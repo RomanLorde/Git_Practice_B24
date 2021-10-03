c4ec6bc0713fa2b0a9c484b4bcc5c4e6ca70e5b8
roma3 <roma38lorde@gmail.com>
2021-10-03 12:45:38 -0400

    third class adding second text

--- src/setup/Third.java
+++ src/setup/Third.java
@@ -4,5 +4,6 @@ public class Third {
     public static void main(String[] args) {
         System.out.println("create the third file");
         System.out.println("Add some text");
+        System.out.println("adiing more text");
     }
 }

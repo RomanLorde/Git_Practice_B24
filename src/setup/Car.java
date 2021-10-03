c8e23d0f049520382003ba6569675d305e983e57
roma3 <roma38lorde@gmail.com>
2021-10-03 16:20:51 -0400

    updating master to origin

--- src/setup/Car.java
+++ src/setup/Car.java
@@ -2,18 +2,18 @@ package setup;
 
 public class Car {
 
-<<<<<<< HEAD
+
     //car class in master branch
 
     //we will have conflict because is different text in diff branches
 
     //more text
 
-=======
+
     // this is our car from james branch
 
     //more text
 
     //more text
->>>>>>> james
+
 }
